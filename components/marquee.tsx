279c9dfca1ce5be5f670a3855416fc8ce4f79e93
Mario Roca <marioccarloss@gmail.com>
2025-08-02 22:37:27 +0200

    feat: hero

--- components/marquee.tsx
+++ components/marquee.tsx
@@ -12,10 +12,10 @@ export const Marquee = ({ className }: { className?: string }) => {
   return (
     <div className={`absolute w-full overflow-hidden ${className}`}>
       <div className='flex w-max animate-marquee p-2 gap-3'>
-        {Array.from({ length: 10 }).map((_, i) => (
+        {Array.from({ length: 15 }).map((_, i) => (
           <MarqueeItem key={i} />
         ))}
-        {Array.from({ length: 10 }).map((_, i) => (
+        {Array.from({ length: 15 }).map((_, i) => (
           <MarqueeItem key={i} />
         ))}
       </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,15 +7,7 @@ const config = {
   ],
   theme: {
     extend: {
-      animation: {
-        marquee: 'marquee 33s linear infinite',
-      },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-      },
+
     },
   },
   plugins: [],
